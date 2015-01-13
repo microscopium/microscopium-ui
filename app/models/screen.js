77524b96802757617dfc3dc69ffdcc612ff8c0ee
Adrian Hecker <starcalibre@gmail.com>
2015-01-13 19:14:21 +1100

    Update mongo schema and add routes

--- app/models/screen.js
+++ app/models/screen.js
@@ -1,26 +1,16 @@
-// mongoose schema for screen object
-
 var mongoose = require('mongoose');
 
 var screenSchema = mongoose.Schema(
-    // specify schema in existing mongo collection
-    {
-        name: String,
-        description: String,
-        no_samples: Number,
-        no_clusters: Number,
-        sample: { // TODO change to sample Schema (eventually)
-            gene_name: String,
-            column: Number,
-            well: String,
-            feature_vector: Array,
-            pca_3d: Array,
-            pca_2d: Array
-        }
-    },
-    // specify collection where objects are
-    {
-        collection: 'interfacetest'
-    });
+  {
+      _id: String, // short screen name < 8 char
+      screen_name: String, // full screen name
+      screen_desc: String,
+      number_samples: Number,
+      samples: Array
+  },
+  {
+        collection: "screens"
+  }
+);
 
 module.exports = mongoose.model('Screen', screenSchema);
--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 var Screen = require('./models/screen');
+var Sample = require('./models/sample');
 
 module.exports = function(app) {
 
@@ -7,12 +8,23 @@ module.exports = function(app) {
         res.sendfile('index.html');
     });
 
-    // get all data from mongo
+    // get all screen documents
     app.get('/api/screens', function(req, res) {
         Screen.find(function(err, data) {
             if (err) res.send(err);
             res.json(data);
         });
+    });
+
+    // get all feature vectors
+    app.get('/api/feature_vectors/:screen', function(req, res) {
+        Sample
+          .find({ screen: req.params.screen })
+          .select('feature_vector')
+          .exec(function(err, feature_vectors) {
+              if (err) res.send(err);
+              res.json(feature_vectors);
+          });
     });
 
     // default route
