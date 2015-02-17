12b66203180098ceed526bf4ec3e51488c61a6a6
Adrian Hecker <strcalibre@gmail.com>
2015-02-17 12:47:24 +1100

    Hide filter until loaded

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,6 +1,11 @@
 function filter(uniqueRow, uniqueCol, uniquePlate, uniqueGene) {
+
+    // append
+
     console.log(uniqueRow);
     console.log(uniqueCol);
     console.log(uniquePlate);
     console.log(uniqueGene);
+
+    $('#filter-button').removeClass('hidden');
 }
