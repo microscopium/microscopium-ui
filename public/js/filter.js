c23dd24535a34a2bf43333d139df3ce554494dfa
Adrian Hecker <strcalibre@gmail.com>
2015-02-17 13:23:07 +1100

    Add filter content

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,11 +1,54 @@
 function filter(uniqueRow, uniqueCol, uniquePlate, uniqueGene) {
 
-    // append
+    var length = uniquePlate.length/2;
+    console.log(length);
+    for(var j = 0; j < 2; j++) {
+        for(var i = 0; i < length; i++) {
+            var el = '#plate-' + (j+1);
+            $('<input />', { type: 'checkbox',
+                name: 'uniquePlate[' + [uniquePlate[i+j*length]] + ']',
+                value: true,
+                checked: true}).appendTo(el);
+            $('<label />', { 'for': uniquePlate[i], text: uniquePlate[i+j*length] }).appendTo(el);
+            $('<br />').appendTo(el);
+        }
+    }
 
-    console.log(uniqueRow);
-    console.log(uniqueCol);
-    console.log(uniquePlate);
-    console.log(uniqueGene);
+    var length = uniqueRow.length/2;
+    console.log(length);
+    for(var j = 0; j < 2; j++) {
+        for(var i = 0; i < length; i++) {
+            var el = '#row-' + (j+1);
+            $('<input />', { type: 'checkbox',
+                name: 'uniqueRow[' + [uniqueRow[i+j*length]] + ']',
+                value: true,
+                checked: true}).appendTo(el);
+            $('<label />', { 'for': uniqueRow[i], text: uniqueRow[i+j*length] }).appendTo(el);
+            $('<br />').appendTo(el);
+        }
+    }
+
+    var length = uniqueCol.length/3;
+    for(var j = 0; j < 3; j++) {
+        for(var i = 0; i < length; i++) {
+            var el = '#col-' + (j+1);
+            $('<input />', { type: 'checkbox',
+                name: 'uniqueCol[' + [uniqueCol[i+j*length]] + ']',
+                value: true,
+                checked: true}).appendTo(el);
+            $('<label />', { 'for': uniqueCol[i], text: uniqueCol[i+j*length] }).appendTo(el);
+            $('<br />').appendTo(el);
+        }
+    }
+
+    for(var i = 0; i < uniqueGene.length; i++) {
+        $('<option />', {
+            value: uniqueGene[i],
+            text: uniqueGene[i]
+        }).appendTo('#gene-select');
+    }
+
+    // TODO make filters actually do stuff
 
     $('#filter-button').removeClass('hidden');
 }
