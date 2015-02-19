271869a8f9fdf0dbd598c52bbee9667714de4752
Adrian Hecker <strcalibre@gmail.com>
2015-02-19 13:42:35 +1100

    Add submit and reset buttons

--- public/js/filter.js
+++ public/js/filter.js
@@ -11,7 +11,7 @@ function SampleFilter(uniqueRow, uniqueCol, uniquePlate, uniqueGene) {
         for(var i = 0; i < length; i++) {
             var el = '#plate-' + (j+1);
             $('<input />', { type: 'checkbox',
-                name: 'uniquePlate[' + [uniquePlate[i+j*length]] + ']',
+                name: 'plate[' + [uniquePlate[i+j*length]] + ']',
                 value: true,
                 checked: true}).appendTo(el);
             $('<label />', { 'for': uniquePlate[i], text: uniquePlate[i+j*length] }).appendTo(el);
@@ -25,7 +25,7 @@ function SampleFilter(uniqueRow, uniqueCol, uniquePlate, uniqueGene) {
         for(var i = 0; i < length; i++) {
             var el = '#row-' + (j+1);
             $('<input />', { type: 'checkbox',
-                name: 'uniqueRow[' + [uniqueRow[i+j*length]] + ']',
+                name: 'rows[' + [uniqueRow[i+j*length]] + ']',
                 value: true,
                 checked: true}).appendTo(el);
             $('<label />', { 'for': uniqueRow[i], text: uniqueRow[i+j*length] }).appendTo(el);
@@ -39,7 +39,7 @@ function SampleFilter(uniqueRow, uniqueCol, uniquePlate, uniqueGene) {
         for(var i = 0; i < length; i++) {
             var el = '#col-' + (j+1);
             $('<input />', { type: 'checkbox',
-                name: 'uniqueCol[' + [uniqueCol[i+j*length]] + ']',
+                name: 'cols[' + [uniqueCol[i+j*length]] + ']',
                 value: true,
                 checked: true}).appendTo(el);
             $('<label />', { 'for': uniqueCol[i], text: uniqueCol[i+j*length] }).appendTo(el);
@@ -49,36 +49,31 @@ function SampleFilter(uniqueRow, uniqueCol, uniquePlate, uniqueGene) {
 
     // attach listener for 'add to filter' event
     $('#filter-add').on('click', function() {
-
-        var value = $('#gene-select').val();
-        var index = self.genes.indexOf(value);
-
-        if(index !== -1 && value) {
-            self.genes.splice(index, 1);
-            self.selectedGenes.push(value);
-            self.updateGeneList();
-            self.updateSelectedGeneList();
-        }
+        self.addToFilter();
     });
 
     // attach listener for 'remove from filter' event
     $('#filter-remove').on('click', function() {
-
-        var value = $('#gene-selected').val();
-        var index = self.selectedGenes.indexOf(value);
-
-        if(index !== -1 && value) {
-            self.selectedGenes.splice(index, 1);
-            self.genes.push(value);
-            self.genes.sort();
-            self.updateGeneList();
-            self.updateSelectedGeneList();
-        }
+        self.removeFromFilter();
     });
 
     // attach listener to gene filter textbox
     $('#gene-filter-text').on('input paste', function() {
         self.updateGeneList();
+    });
+
+    // attach listener to submit button
+    $('#filter-form').on('submit', function(event) {
+        event.preventDefault();
+        var filterQuery = $('form').serializeJSON();
+        // add genes to query
+        filterQuery['gene_list'] = self.selectedGenes;
+        console.log(filterQuery);
+    });
+
+    // attach additional behavior to reset button
+    $('#filter-form').on('reset', function() {
+        self.resetGeneFilter();
     });
 
     // display filter button once components mounted
@@ -87,8 +82,41 @@ function SampleFilter(uniqueRow, uniqueCol, uniquePlate, uniqueGene) {
 
 }
 
-SampleFilter.prototype.updateGeneList = function() {
+SampleFilter.prototype.addToFilter = function() {
+    var value = $('#gene-select').val();
+    var index = this.genes.indexOf(value);
 
+    if(index !== -1 && value) {
+        this.genes.splice(index, 1);
+        this.selectedGenes.push(value);
+        this.updateGeneList();
+        this.updateSelectedGeneList();
+    }
+};
+
+SampleFilter.prototype.removeFromFilter = function() {
+    var value = $('#gene-selected').val();
+    var index = this.selectedGenes.indexOf(value);
+
+    if(index !== -1 && value) {
+        this.selectedGenes.splice(index, 1);
+        this.genes.push(value);
+        this.genes.sort();
+        this.updateGeneList();
+        this.updateSelectedGeneList();
+    }
+};
+
+SampleFilter.prototype.resetGeneFilter = function() {
+    $('#gene-selected').children().remove();
+    this.genes.push.apply(this.genes, this.selectedGenes);
+    this.genes.sort();
+    this.selectedGenes = [];
+    this.updateGeneList();
+};
+
+SampleFilter.prototype.updateGeneList = function() {
+    console.log('updateGeneList');
     $('#gene-select').children().remove();
 
     var pattern = $('#gene-filter-text').val();
@@ -114,12 +142,10 @@ SampleFilter.prototype.updateSelectedGeneList = function() {
 
     for(var i = 0; i < this.selectedGenes.length; i++) {
         $('<option />', {
-            value: this.selectedGenes[i],
             text: this.selectedGenes[i]
         }).appendTo('#gene-selected');
     }
 };
-
 
 function regexFilter(pattern) {
     return function(element) {
