8b1d4d648d4c1f971638a7e57e61321d3534da4e
Adrian Hecker <strcalibre@gmail.com>
2015-02-20 13:15:51 +1100

    Add browserify to grunt pipeline

--- gruntfile.js
+++ gruntfile.js
@@ -16,10 +16,19 @@ module.exports = function(grunt) {
                 force: true
             }
         },
+        browserify: {
+            dev: {
+                options: {
+                    debug: true
+                },
+                src: ['public/js/*.js'],
+                dest: 'public/js/build.js'
+            }
+        },
         watch: {
             scripts: {
                 files: ['*.js', 'public/js/*.js'],
-                tasks: ['jshint', 'express:dev'],
+                tasks: ['jshint', 'browserify:dev', 'express:dev'],
                 options: {
                     livereload: true,
                     spawn: false
@@ -30,8 +39,9 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['express:dev', 'jshint:all', 'watch']);
+    grunt.registerTask('default', ['express:dev', 'browserify:dev', 'jshint:all', 'watch']);
 
 };
