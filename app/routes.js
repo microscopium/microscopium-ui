2ce928c583c0d8fc9cc3cddc1645ee06a45919a6
Adrian Hecker <starcalibre@gmail.com>
2015-05-12 16:08:20 +1000

    Update routes * Simplify some routes and removed unused * Add a route to query the features collection in the DB

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,10 @@
+var Feature = require('./models/feature');
 var Screen = require('./models/screen');
 var Sample = require('./models/sample');
 var Image = require('./models/image');
+
+var _ = require('lodash');
+_.mixin(require('lodash-deep'));
 
 var resHandler = function(res, sort) {
     if(sort === true) {
@@ -24,19 +28,12 @@ module.exports = function(app) {
         res.sendfile('index.html');
     });
 
-    // get all screen ids
-    app.get('/api/screen_ids', function(req, res) {
+    // get fields of all screens
+    app.get('/api/screens/:field/', function(req, res) {
         Screen
             .find({})
-            .select('_id')
+            .select(req.params.field)
             .exec(resHandler(res));
-    });
-
-    // find unique values for given screen and field
-    app.get('/api/unique/:screen/:field', function(req, res) {
-        Sample
-            .find({'screen': req.params.screen})
-            .distinct(req.params.field, resHandler(res, true));
     });
 
     // get specific screen document
@@ -46,43 +43,50 @@ module.exports = function(app) {
             .exec(resHandler(res));
     });
 
+    // get specific field from screen document
+    app.get('/api/screen/:id/:field', function(req, res) {
+        Screen
+            .find({'_id': req.params.id})
+            .select(req.params.field)
+            .exec(resHandler(res));
+    });
+
     // get all samples belonging to screen
     app.get('/api/samples/:screen', function(req, res) {
         Sample
         .find({ 'screen': req.params.screen })
-        .select('_id row column plate gene_name feature_vector_std ' +
+        .select('_id row column plate gene_name ' +
                 'pca_vector neighbours')
         .exec(resHandler(res));
     });
 
-    // get specific sample
-    app.get('/api/sample/:id', function(req, res) {
-          Sample
-            .find({ '_id': req.params.id })
-            .exec(resHandler(res))
-    });
-
-    // get all neighbours
-    app.get('/api/sample/neighbours/:id', function(req, res) {
-        Sample
-            .find({ '_id': req.params.id })
-            .select('neighbours')
-            .exec(function(err, data) {
-                Sample.find({
-                    '_id': { $in: data[0].neighbours }
-                })
-                .exec(function(err, data) {
-                    res.send(data);
-                });
-            })
-    });
-
     // get value from sample document
-    app.get('/api/sample/:key/:id', function(req, res) {
+    app.get('/api/sample/:id/:field', function(req, res) {
       Sample
         .find({ '_id': req.params.id })
-        .select(req.params.key)
+        .select(req.params.field)
         .exec(resHandler(res))
+    });
+
+    // get specific field from screen document
+    // parameter slice - only get specific element of array
+    app.get('/api/samples/:screen/:field', function(req, res) {
+        Sample
+            .find({'screen': req.params.screen})
+            .select(req.params.field)
+            .exec(function(err, data) {
+                if(req.query.pluck) {
+                    if(err) {
+                        res.send(err);
+                    }
+                    var pluck = _.deepPluck(data,
+                        [req.params.field, req.query.pluck]);
+                    res.send(pluck);
+                }
+                else {
+                    res.send(data);
+                }
+            });
     });
 
     //get large image
@@ -106,6 +110,17 @@ module.exports = function(app) {
                     .select('sample_id image_thumb')
                     .exec(resHandler(res));
             })
+    });
+
+    // get feature distribution
+    app.get('/api/feature/:screen/:feature/', function(req, res) {
+        Feature
+            .find({
+                'screen': req.params.screen,
+                'feature_name': req.params.feature
+            })
+            .select('feature_dist_std')
+            .exec(resHandler(res))
     });
 
     // default route
