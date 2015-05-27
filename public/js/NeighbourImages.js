4d5006603bdf93fb187d2d1a032fedaa8b4408ea
Adrian Hecker <starcalibre@gmail.com>
2015-05-27 13:31:37 +1000

    Expand upon a few of the code comments * Some additional explanations added to make some of the design choices a little more clear

--- public/js/NeighbourImages.js
+++ public/js/NeighbourImages.js
@@ -6,22 +6,26 @@ var _ = require('lodash');
  * @constructor
  */
 function NeighbourImages() {
+    var self = this;
     this.neighbours = [];
     this.selectedImage = "";
     this.neighbourImages = [];
 
-    // cache jquery selectors used frequency by gallery methods
-    // call these in the constructor so the DOM is traversed only once
-    // before caching.
+    // When a jQuery selector is called, the DOM must be traversed in order
+    // to find the element. if the selector is saved to a variable (ie cached), the
+    // element is remembered thus the DOM doesn't need to be traversed on subsequent
+    // calls. Frequently used selectors are cached in the object constructor so the
+    // selectors are cached when the object is created.
     this.selectors = {
         $nebula0: $('#nebula-0'),
         // create an array of jquery selectors for all the neighbour images
         $nebula: _.map(_.range(1, 24), function(value) {
-            return $('#nebula-' + (value))
+            return $('#nebula-' + (value));
         }),
         $body:  $('body'),
         $imgZoomButton: $('#img-zoom-button'),
-        $thumbnailChildImg: $('.thumbnail > img')
+        $thumbnailChildImg: $('.thumbnail > img'),
+        $imageButtons: $('#image-buttons')
     };
 
     var divWidth = Math.round($('#image-column').width());
@@ -37,17 +41,17 @@ function NeighbourImages() {
 
     // hide/unhide image buttons on hover
     $('#main-image').hover(function() {
-        $('#image-buttons').removeClass('hidden');
+        self.selectors.$imageButtons.removeClass('hidden');
     }, function() {
-        $('#image-buttons').addClass('hidden');
+        self.selectors.$imageButtons.addClass('hidden');
     });
 }
 
 /**
  * getImages: Get images for selected sample.
  *
- * Sends two AJAX requests to get the medium size image of the
- * inputed sample_id, and to get the images of the neighbouring
+ * Send two AJAX requests to get the medium size image of the
+ * input sample_id, and the images of the neighbouring
  * samples.
  *
  * @this {NeighbourImages}
@@ -58,12 +62,13 @@ NeighbourImages.prototype.getImages = function(sample_id) {
 
     var imageQueryLarge = {
         sample_id: sample_id,
-        select: ['image_large sample_id']
+        select: ['image_large sample_id'],
+        include: ['gene_name']
     };
 
     var imageQueryNeighbour = {
         sample_id: sample_id,
-        select: ['image_thumb sample_id'],
+        select: ['image_thumb sample_id gene_name'],
         neighbours: true
     };
 
@@ -82,7 +87,7 @@ NeighbourImages.prototype.getImages = function(sample_id) {
 
         })).then(function() {
 
-            // matches the height of the image gallery to the plots
+            // match the height of the image gallery to the plots..
             // do this *after* the images are loaded from the DB
             var height = Math.round($('#plot-column').height());
             $('#image-column')
@@ -103,7 +108,10 @@ NeighbourImages.prototype.getImages = function(sample_id) {
 NeighbourImages.prototype.setImages = function() {
     var self = this;
 
-    // make all img frames empty
+    // make all image frames empty, this is done to prevent
+    // images belonging to the previous sample selected
+    // from displaying if something goes wrong when the
+    // images are loaded.
     this.selectors.$nebula0
         .attr('src', '//:0')
         .attr('title', 'not found');
@@ -117,7 +125,8 @@ NeighbourImages.prototype.setImages = function() {
         this.selectors.$nebula0
             .attr('src', 'data:image/jpg;base64,' +
                 this.selectedImage.image_large)
-            .attr('title', this.selectedImage.sample_id);
+            .attr('title', this.selectedImage.sample_id + '&#013;'
+                + this.selectedImage.gene_name);
     }
 
     this.selectors.$imgZoomButton.unbind('click');
@@ -135,8 +144,9 @@ NeighbourImages.prototype.setImages = function() {
             .attr('title', self.neighbourImages[i].sample_id);
         this.selectors.$nebula[i].unbind('click');
 
-        // use IIFE here as closure being called in a loop
-        // see http://www.mennovanslooten.nl/blog/post/62
+        // use IIFE (immediately invoked function expression)
+        // here as a closure is being called in a loop
+        // see http://www.mennovanslooten.nl/blog/post/62 for additional details
         (function(j) {
             self.selectors.$nebula[i].on('click', function(event) {
                 event.preventDefault();
@@ -150,8 +160,8 @@ NeighbourImages.prototype.setImages = function() {
 /**
  * openFullModal: Open the modal that displays the full size image.
  *
- * Sends an AJAX request to get the full-size image, sets the content of
- * the image modal and opens it.
+ * Send an AJAX request to get the full-size image and set the content of
+ * the image modal before opening it.
  *
  * @this {NeighbourImages}
  */
@@ -164,7 +174,7 @@ NeighbourImages.prototype.openFullModal = function() {
         type: 'GET',
         url: '/api/images/?' + $.param(imageQuery),
         success: function(data) {
-            // TODO some collections don't have a fullsize image, use the large image in lieu
+            // TODO remove once all collections reliably have a fullsize image
             var image = data[0].image_full || data[0].image_large;
 
             // add image title
