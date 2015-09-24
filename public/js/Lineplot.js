8a8524669e1062ff853fc725778296ee4596f0b6
Adrian Hecker <starcalibre@gmail.com>
2015-09-24 14:49:45 +1000

    Refactor lineplot object * Break up larger functions into smaller methods * Use private methods where applicable * Reduce use of var self = this

--- public/js/Lineplot.js
+++ public/js/Lineplot.js
@@ -24,12 +24,11 @@ var RIGHTARROW = 39;
  * @constructor
  * @param {string} element - The ID of the target div for this plot.
  */
-function Lineplot(screen, element) {
+function Lineplot(element) {
     // this variable refers to the position of the vertical 'active feature'
     // line on the plot, *not* the index of the feature in the feature vector.
     this.activeFeature = 1;
     this.element = element;
-    this.screen = screen;
 
     this.xAxisTicks = config.xAxisTicks;
     this.yAxisTicks = config.yAxisTicks;
@@ -39,24 +38,25 @@ function Lineplot(screen, element) {
     var aspectWidth = config.aspectRatio.width;
     var aspectHeight = config.aspectRatio.height;
     this.fullWidth = $(this.element).width();
-    this.fullHeight = Math.round(this.fullWidth * (aspectHeight/aspectWidth));
+    this.fullHeight = Math.round(this.fullWidth *
+        (aspectHeight / aspectWidth));
 
-    this.width = this.fullWidth - this.margin.left - this.margin.right;
-    this.height = this.fullHeight - this.margin.top - this.margin.bottom;
+    this.width = this.fullWidth - this.margin.left -
+        this.margin.right;
+    this.height = this.fullHeight - this.margin.top -
+        this.margin.bottom;
 }
 
 /**
- * onClickUpdate: Get feature vector for sample and update lineplot.
+ * drawLineplot: Draw the lineplot.
  *
- * Once the data has been fetched from the database, it is sent to
- * the drawLineplot method to update the plot.
+ * Make an ajax request to get the data for the currently selected
+ * sample and draw the lineplot.
  *
  * @this {Lineplot}
- * @param {string} sampleId - The _id of the sample to get from
- *     the database.
+ * @param {string} sampleId - The sample ID to display in the lineplot.
  */
-Lineplot.prototype.getSampleData = function(sampleId) {
-    var self = this;
+Lineplot.prototype.drawLineplot = function(sampleId) {
     var query = {
         id: sampleId,
         select: 'feature_vector_std'
@@ -65,139 +65,146 @@ Lineplot.prototype.getSampleData = function(sampleId) {
         type: 'GET',
         url: '/api/samples/?' + $.param(query),
         success: function(data) {
-            self.drawLineplot(data[0]);
-        }
+            this.featureVector = data[0].feature_vector_std;
+            d3.select(this.element + ' > svg').remove(); // clear canvas
+            this._addBackground();
+            this._setScale();
+            this._drawAxis();
+            this._drawLine();
+            this._drawTitle(data[0]._id);
+            this._addEventListeners();
+            this._updateActiveLine(this.activeFeature);
+        }.bind(this)
     });
 };
 
 /**
- * drawLineplot: Draw the lineplot.
+ * addBackground: Add SVG container to hold plot elements.
  *
- * @this {Lineplot}
- * @param {object} sampleData - An object containing two properties:
- *     * _id: A string, the id of the sample to be drawn.
- *     * feature_vector_std - An array, the standardised feature vector
- *       of the sample.
+ * An SVG container is added to draw all the plot elements.
+ * The SVG container alone cannot catch click events, so a
+ * solid white 'background' is added to the container to
+ * catch these click events.
+ *
+ * This method should be called first.
+ *
+ * @private
  */
-Lineplot.prototype.drawLineplot = function(sampleData) {
-    var self = this;
-    this.featureVector = sampleData.feature_vector_std;
-
-    self.destroy();
-
-    // get min/max x/y values -- needed for scaling axis/data
-    var yMin = d3.min(this.featureVector);
-    var yMax = d3.max(this.featureVector);
-
-    // var create (x, y) pairs for plot
-    var linePoints = _.zip(_.range(1, this.featureVector.length+1),
-        this.featureVector);
-
-    // define x-scale and x-axis
-    self.xScale = d3.scale.linear()
-        .domain([0, this.featureVector.length])
-        .range([0, self.width]);
-
-    var xAxis = d3.svg.axis()
-        .scale(self.xScale)
-        .orient('bottom')
-        .ticks(self.xAxisTicks);
-
-    // create margin
-    var yMargin = (yMax-yMin)*this.axisMargin;
-
-    self.yScale = d3.scale.linear()
-        .domain([yMin-yMargin, yMax+yMargin])
-        .range([self.height, 0]);
-
-    var yAxis = d3.svg.axis()
-        .scale(self.yScale)
-        .orient('left')
-        .ticks(self.yAxisTicks);
-
-    var line = d3.svg.line()
-        .x(function(d) { return self.xScale(d[0]); })
-        .y(function(d) { return self.yScale(d[1]); });
-
+Lineplot.prototype._addBackground = function() {
     // append canvas
-    self.svg = d3.select(this.element).append('svg')
-        .attr('width', self.fullWidth)
-        .attr('height', self.fullHeight)
+    this.svg = d3.select(this.element).append('svg')
+        .attr('width', this.fullWidth)
+        .attr('height', this.fullHeight)
         .append('g')
-        .attr('transform', 'translate(' + self.margin.left + ',' + self.margin.top + ')')
-        .on('click', function () {
-            d3.event.stopPropagation();
-            self.onClickUpdate(d3.mouse(this));
-        });
+        .attr('transform', 'translate(' + this.margin.left + ',' +
+            this.margin.top + ')');
 
     // append background svg
-    self.svg.append('rect')
+    this.svg.append('rect')
         .attr('x', 0)
         .attr('y', 0)
         .attr('width', this.fullWidth)
         .attr('height', this.fullHeight)
-        .style('fill', 'white')
-        .on('click', function () {
-            d3.event.stopPropagation();
-            self.onClickUpdate(d3.mouse(this));
-        });
-
-    // append listener to svg element
-    d3.select('body').on('keydown', function() {
-        self.keypressUpdate(d3.event.keyCode);
-    });
-
-    // add x-axis
-    self.svg.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + self.height + ')')
-        .call(xAxis);
-
-    // add y-axis
-    self.svg.append('g')
-        .attr('class', 'y axis')
-        .call(yAxis);
-
-    // add line graphic
-    self.svg.append('path')
-        .datum(linePoints)
-        .attr('class', 'line')
-        .attr('d', line);
-
-    // add title
-    self.svg.append('text')
-        .attr('x', self.width/2)
-        .attr('y', -5)
-        .style('text-anchor', 'middle')
-        .text(sampleData._id);
-
-    // set active line
-    self.updateActiveLine(this.activeFeature);
+        .style('fill', 'white');
 };
 
 /**
- * updateActiveLine: Redraw the line overlaying selected feature in the sample.
+ * addEventListeners: Add event listener behaviour to plot.
  *
- * Re-draw the active feature line and updates the histogram for the
- * corresponding feature.
- *
- * @this {Lineplot}
- * @param {number} feature - The position on the x-axis where the active
- *    feature line should be drawn.
+ * @private
  */
-Lineplot.prototype.updateActiveLine = function(feature) {
+Lineplot.prototype._addEventListeners = function() {
     var self = this;
-    var xCoord = self.xScale(feature);
-    self.svg.selectAll('.selectedLine').remove();
-    self.svg.append('line')
-        .attr('x1', xCoord)
-        .attr('y1', 0)
-        .attr('x2', xCoord)
-        .attr('y2', self.height)
-        .attr('stroke', 'red')
-        .attr('stroke-width', 0.75)
-        .attr('stroke-dasharray', '5, 5')
-        .classed('selectedLine', true);
+
+    // catch any keydown events -- this is added to
+    // body of the DOM so events are caught even if
+    // focus is on another DOM element.
+    d3.select('body').on('keydown', function() {
+        d3.event.stopPropagation();
+        self._onKeydownUpdate(d3.event.keyCode);
+    });
+
+    // catch all click events on the container svg
+    this.svg.on('click', function() {
+        d3.event.stopPropagation();
+        self._onClickUpdate(d3.mouse(this));
+    });
+};
+
+/**
+ * drawAxis: Draw the X and Y axis on the plot.
+ *
+ * The scale should be set before this method is called.
+ *
+ * See: _setScale
+ *
+ * @private
+ */
+Lineplot.prototype._drawAxis = function() {
+    var xAxis = d3.svg.axis()
+        .scale(this.xScale)
+        .orient('bottom')
+        .ticks(this.xAxisTicks);
+
+    var yAxis = d3.svg.axis()
+        .scale(this.yScale)
+        .orient('left')
+        .ticks(this.yAxisTicks);
+
+    this.svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + this.height + ')')
+        .call(xAxis);
+
+    this.svg.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis);
+};
+
+/**
+ * drawLine: Draw the line for the lineplot.
+ *
+ * @private
+ */
+Lineplot.prototype._drawLine = function() {
+    var linePoints = _.zip(_.range(1, this.featureVector.length + 1),
+        this.featureVector);
+
+    var line = d3.svg.line()
+        .x(function(d) { return this.xScale(d[0]); }.bind(this))
+        .y(function(d) { return this.yScale(d[1]); }.bind(this));
+
+    this.svg.append('path')
+        .datum(linePoints)
+        .attr('class', 'line')
+        .attr('d', line);
+};
+
+/**
+ * drawTitle: Add a title to the lineplot.
+ *
+ * @param {string} titleText - The text to draw in the title.
+ * @private
+ */
+Lineplot.prototype._drawTitle = function(titleText) {
+    this.svg.append('text')
+        .attr('x', this.width / 2)
+        .attr('y', -this.margin.top / 4)
+        .style('text-anchor', 'middle')
+        .text(titleText);
+};
+
+/**
+ * handleFeatureChange: Handle updates when active feature changes.
+ *
+ * Re-draw the line showing which feature is currently active and
+ * broadcase the 'updateLineplot' event to the UI.
+ *
+ * @private
+ */
+Lineplot.prototype._handleFeatureChange = function() {
+    this._updateActiveLine();
+    $('body').trigger('updateLineplot', this.activeFeature);
 };
 
 /**
@@ -209,56 +216,90 @@ Lineplot.prototype.updateActiveLine = function(feature) {
  * @this {Lineplot}
  * @param {d3Mouse} d3Mouse - Mouse object generated by d3 when a onclick
  *     event is triggered.
+ * @private
  */
-Lineplot.prototype.onClickUpdate = function (d3Mouse) {
-    var self = this;
+Lineplot.prototype._onClickUpdate = function (d3Mouse) {
     var xCoord = d3Mouse[0];
-    var clickedFeature = Math.round(self.xScale.invert(xCoord));
+    var clickedFeature = Math.round(this.xScale.invert(xCoord));
 
-    if(clickedFeature <= self.featureVector.length && clickedFeature > 1) {
-        self.activeFeature = clickedFeature;
-        $('body').trigger('updateLineplot', self.activeFeature);
+    if(clickedFeature <= this.featureVector.length && clickedFeature > 1) {
+        this.activeFeature = clickedFeature;
+        this._handleFeatureChange();
     }
 };
 
 /**
- * keypressUpdate: Update active feature line on keypress.
+ * onKeydownUpdate: Update active feature line on keypress.
  *
  * On pressing the left or right arrow key, the active feature
- * line and feature histogram is updated.
+ * is updated. This function checks that the change is valid
+ * and handles the update.
  *
  * @this {Lineplot}
  * @param {number} keyCode - The Javascript character code for the
  *     pressed key.
  *     See https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+ * @private
  */
-Lineplot.prototype.keypressUpdate = function(keyCode) {
-    var self = this;
-
-    if(self.activeFeature) {
-        if(keyCode === RIGHTARROW &&
-            // only let the line move right if it's not already at the
-            // rightmost feature
-            self.activeFeature < this.featureVector.length) {
-            self.activeFeature++;
-            $('body').trigger('updateLineplot', self.activeFeature);
-        }
-        else if(keyCode === LEFTARROW && self.activeFeature > 1) {
-            // only let the line move left if it's not already at the
-            // left-most feature
-            self.activeFeature--;
-            $('body').trigger('updateLineplot', self.activeFeature);
-        }
+Lineplot.prototype._onKeydownUpdate = function(keyCode) {
+    // only let the line move right if it's not already at the
+    // rightmost feature
+    if(keyCode === RIGHTARROW &&
+        this.activeFeature < this.featureVector.length) {
+        this.activeFeature++;
+        this._handleFeatureChange();
+    }
+    // only let the line move left if it's not already at the
+    // left-most feature
+    else if(keyCode === LEFTARROW && this.activeFeature > 1) {
+        this.activeFeature--;
+        this._handleFeatureChange();
     }
 };
 
 /**
- * destroy: Remove all child SVG elements of the plot objects containing div.
+ * setScale: Set scale for current feature vector.
+ *
+ * Sets the scale that maps the data to the plot.
+ *
+ * @private
+ */
+Lineplot.prototype._setScale = function() {
+    // get min/max x/y values -- needed for scaling axis/data
+    var yMin = d3.min(this.featureVector);
+    var yMax = d3.max(this.featureVector);
+
+    // create margin for y axis -- stops the line from
+    // drawing at the very top or bottom of the plot
+    var yMargin = (yMax - yMin) * this.axisMargin;
+
+    this.xScale = d3.scale.linear()
+        .domain([0, this.featureVector.length])
+        .range([0, this.width]);
+
+    this.yScale = d3.scale.linear()
+        .domain([yMin - yMargin, yMax + yMargin])
+        .range([this.height, 0]);
+};
+
+/**
+ * updateActiveLine: Redraw the line overlaying selected feature in the plot.
  *
  * @this {Lineplot}
+ * @private
  */
-Lineplot.prototype.destroy = function() {
-    d3.select(this.element + ' > svg').remove();
+Lineplot.prototype._updateActiveLine = function() {
+    var xCoord = this.xScale(this.activeFeature);
+    this.svg.selectAll('.selectedLine').remove();
+    this.svg.append('line')
+        .attr('x1', xCoord)
+        .attr('y1', 0)
+        .attr('x2', xCoord)
+        .attr('y2', this.height)
+        .attr('stroke', 'red')
+        .attr('stroke-width', 0.75)
+        .attr('stroke-dasharray', '5, 5')
+        .classed('selectedLine', true);
 };
 
 module.exports = Lineplot;
