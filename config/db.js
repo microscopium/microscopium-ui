bd0707ccf7be01f32363f957fe331d8f74aab67b
Adrian Hecker <starcalibre@gmail.com>
2015-11-18 10:07:30 +1100

    Remove all node code

--- config/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // the database url
-    url : process.env.MONGO_URL || 'mongodb://localhost/microscopium',
-    mongoUser: process.env.MONGO_USER,
-    mongoPassword: process.env.MONGO_PASSWORD
-};
