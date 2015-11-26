d7230a86d7d649f0abe8becb55d6499a5078f87e
Adrian Hecker <starcalibre@gmail.com>
2015-11-27 10:18:14 +1100

    Update DB API routes * Use new agreed upon routes * Add docstrings to route functions
    Add additional examples to route docstrings

--- app/dbapi/views.py
+++ app/dbapi/views.py
@@ -1,10 +1,30 @@
+# using bson.json_util here as json and ujson can't serialise mongo cursors
 from bson.json_util import dumps
+
 from flask import request
-from app.dbapi import dbapi
+from . import dbapi
 from app import mongo
 
-@dbapi.route("/images/<sample_id>")
-def get_images(sample_id):
+@dbapi.route("/<screen_id>/samples/<sample_id>/images")
+def get_images(screen_id, sample_id):
+    """Get an image from the images collection.
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    e.g /api/images/BBBC017-50115070001-A01?select=image_large
+
+    Parameters
+    ----------
+    screen_id : str
+        The screen _id the image belongs to.
+    sample_id : str
+        The sample _id of the image to find.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+    """
     find_query = {"sample_id": sample_id}
     select_query = request.args.getlist("select")
 
@@ -13,8 +33,27 @@ def get_images(sample_id):
     return dumps(result)
 
 
-@dbapi.route("/images/<sample_id>/neighbours")
-def get_images_neighbours(sample_id):
+@dbapi.route("/<screen_id>/samples/<sample_id>/images/neighbours")
+def get_images_neighbours(screen_id, sample_id):
+    """Get the neighbouring images for a sample from the images collection.
+
+    e.g /api/images/BBBC017-50115070001-A01/neighbours?select=image_thumb
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    Parameters
+    ----------
+    screen_id : str
+        The screen _id the image belongs to.
+    sample_id : str
+        The sample _id of the image to find.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+        Note: Binary data will be returned in the "$binary" subfield.
+    """
     find_query = {"_id": sample_id}
     select_query = request.args.getlist("select")
 
@@ -29,8 +68,24 @@ def get_images_neighbours(sample_id):
     return dumps(result)
 
 
-@dbapi.route("/features/<screen_id>/<feature>")
+@dbapi.route("/<screen_id>/features/<feature>")
 def get_feature(screen_id, feature):
+    """Get the feature distribution from a screen.
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    Parameters
+    ----------
+    screen_id : str
+        The screen _id of the feature to find.
+    feature : str
+        The feature_name of the feature to find.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+    """
     find_query = {"screen": screen_id, "feature_name": feature}
     select_query = request.args.getlist("select")
 
@@ -39,8 +94,24 @@ def get_feature(screen_id, feature):
     return dumps(result)
 
 
-@dbapi.route("/samples/<screen_id>")
-def get_sample(screen_id):
+@dbapi.route("/<screen_id>/samples")
+def get_samples(screen_id):
+    """Get all samples belonging to a particular screen.
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    e.g. /api/BBBC017/samples?select=feature_vector_std
+
+    Parameters
+    ----------
+    screen_id : str
+        The screen _id that the samples belong to.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+    """
     find_query = {"screen": screen_id}
     select_query = request.args.getlist("select")
 
@@ -49,8 +120,22 @@ def get_sample(screen_id):
     return dumps(result)
 
 
-@dbapi.route("/samples/<screen_id>/<sample_id>")
-def get_samples(screen_id, sample_id):
+@dbapi.route("/<screen_id>/samples/<sample_id>")
+def get_sample(screen_id, sample_id):
+    """Get a sample from from the samples collection.
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    Parameters
+    ----------
+    screen_id : str
+        The query sample _id.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+    """
     find_query = {"screen": screen_id, "_id": sample_id}
     select_query = request.args.getlist("select")
 
@@ -60,8 +145,22 @@ def get_samples(screen_id, sample_id):
 
 
 # this route is defined for consistency with the "/images/<sample_id>/neighbours" route
-@dbapi.route("/samples/<screen_id>/<sample_id>/neighbours")
+@dbapi.route("/<screen_id>/samples/<sample_id>/neighbours")
 def get_samples_neighbours(screen_id, sample_id):
+    """Get the neighbouring samples for a sample in the collection.
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    Parameters
+    ----------
+    sample_id : str
+        The query sample _id.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+    """
     find_query = {"screen": screen_id, "_id": sample_id}
 
     result = mongo.db.samples.find(find_query,
@@ -72,6 +171,15 @@ def get_samples_neighbours(screen_id, sample_id):
 
 @dbapi.route("/screens")
 def get_screens():
+    """Get all screens from the screens collection.
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+    """
     select_query = request.args.getlist("select")
 
     result = mongo.db.screens.find({}, fields=select_query)
@@ -81,6 +189,22 @@ def get_screens():
 
 @dbapi.route("/screens/<screen_id>")
 def get_screen(screen_id):
+    """Get a screen from the screens collection.
+
+    Fields to return should be specified using the "select" URL parameter.
+
+    e.g. /api/screens/BBBC017?select=screen_desc&select=screen_features
+
+    Parameters
+    ----------
+    screen_id : string
+        The _id of the query screen.
+
+    Returns
+    -------
+    result : string
+        The result of the database call as a JSON string.
+    """
     find_query = {"_id": screen_id}
     select_query = request.args.getlist("select")
 
