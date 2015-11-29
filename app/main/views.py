f98b82840b6a4f0dd8da705f48c10bab1fb017ae
Adrian Hecker <starcalibre@gmail.com>
2015-11-30 10:25:35 +1100

    Add after_request decorator to minify HTML
    Add HTML/CSS/JS minification options to config file
    
    Remove unused MINIFY_CSS and MINIFY_JSS options
    * These are to be used in a later pull request

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,16 @@
 from flask import render_template
 
+from htmlmin.main import minify
+
 from app.main import main
 
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.after_request
+def minify_html_response(response):
+    if response.content_type == u"text/html; charset=utf-8":
+        response.set_data(minify(response.get_data(as_text=True)))
+        return response
+    return response
--- config.py
+++ config.py
@@ -5,10 +5,17 @@ See Flask docs: http://flask.pocoo.org/docs/0.10/config/
 """
 
 class Config:
-    pass
+    DEBUG = True
+
+    # If true, minified HTML will be sent to the browser.
+    # This should be False during development
+    MINIFY_HTML = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+    MINIFY_HTML = False
+
     MONGO_DBNAME = "microscopium"
 
 config = {
