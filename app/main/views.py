500b6f8ba73578a2fa74dd9c1badcaf0066ca60f
Adrian Hecker <starcalibre@gmail.com>
2015-11-30 10:51:48 +1100

    Update main routes to work with new template files * Add docstrings for each route * Minify HTML based on the config option

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,33 @@
-from flask import render_template
+from flask import current_app, render_template
 
 from htmlmin.main import minify
 
 from app.main import main
+from .. import mongo
 
 @main.route("/")
 def index():
-    return render_template("index.html")
+    """Render index.html.
+    """
+    screens = mongo.db.screens.find()
+    return render_template("index.html", screens=screens)
+
+@main.route("/<screen_id>")
+def load_screen(screen_id):
+    """Render UI html and send back in response.
+
+    The screen_id parameter is passed to the JavaScript variable
+    SCREEN_ID in the pages HTML.
+    """
+    return render_template("screen_ui.html",
+                           screen_id=screen_id)
 
 @main.after_request
 def minify_html_response(response):
-    if response.content_type == u"text/html; charset=utf-8":
+    """Minify any responses that contain HTML.
+    """
+    if response.content_type == u"text/html; charset=utf-8" and \
+            current_app.config["MINIFY_HTML"]:
         response.set_data(minify(response.get_data(as_text=True)))
         return response
     return response
